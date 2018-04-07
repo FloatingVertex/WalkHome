67502ad19b110b93ad10a3b0f052f2656dcd9ec7
Tom Slavin <tps3871@g.rit.edu>
2018-04-07 14:53:12 -0400

    Obstacle updates

---
+++ Assets/Scripts/BouncyPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private float launchForce = 10;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.gameObject.layer == 8) || (collision.gameObject.layer == 9))
+        {
+            Debug.Log(Vector2.Dot(collision.contacts[0].normal, Vector2.down));
+            if (Vector2.Dot(collision.contacts[0].normal, Vector2.down) > 0)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, launchForce));
+            }
+        }
+    }
+}
