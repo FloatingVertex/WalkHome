525b03ff5f9c1ac58359cd94950a1c50e7ef41a2
Dust Cloud <atlantishelf3@gmail.com>
2018-04-07 11:37:48 -0400

    Basic Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class PlayerController : MonoBehaviour {
+
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+
+    private void Update()
+    {
+        int movement = 0;
+        if(Input.GetKey(leftKey))
+        {
+            movement += -1;
+        }
+        if(Input.GetKey(rightKey))
+        {
+            movement += 1;
+        }
+        GetComponent<Character>().Movement = movement;
+    }
+}
