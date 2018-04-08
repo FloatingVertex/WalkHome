d529f4616fa43d8050fda0336fbcd1586d3dcf8a
FloatingVertex <pushpindersingh135@gmail.com>
2018-04-08 17:42:43 -0400

    Fixed a bug

--- Assets/Scripts/EndingDialogTrigger.cs
+++ Assets/Scripts/EndingDialogTrigger.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndingDialogTrigger : MonoBehaviour {
+public class EndingDialogTrigger : EventTrigger {
 
-	// Use this for initialization
-	void Start () {
+    public EventTrigger event2;
+    public EventTrigger event3;
+    public EventTrigger event4;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    public override void RunTrigger()
+    {
+        var hs = HappinessManager.singleton.happyness;
+        if (hs >= 4 && hs < 7)
+        {
+            event2.RunTrigger();
+        }else if (hs < 10)
+        {
+            event3.RunTrigger();
+        }else
+        {
+            event4.RunTrigger();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
