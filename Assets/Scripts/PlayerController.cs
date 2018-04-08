42752c29ada5ddd0d809ec209ec3b3bc17076f16
FloatingVertex <pushpindersingh135@gmail.com>
2018-04-08 14:41:35 -0400

    Player can nolonger climb

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour {
     public KeyCode climbDownKey = KeyCode.S;
     public KeyCode jumpKey = KeyCode.Space;
 
+    private bool onJumpableService = false;
+
     private void Update()
     {
         int movement = 0;
@@ -31,8 +33,25 @@ public class PlayerController : MonoBehaviour {
         {
             climbing += -1;
         }
-        GetComponent<Character>().jump = Input.GetKey(jumpKey);
+        GetComponent<Character>().jump = (Input.GetKey(jumpKey) && onJumpableService);
+        if((Input.GetKey(jumpKey) && onJumpableService)) { onJumpableService = false; }
         GetComponent<Character>().Movement = movement;
         GetComponent<Character>().ClimbState = climbing;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach(var cp in collision.contacts)
+        {
+            if(Mathf.Abs(Vector2.Dot(cp.normal,Vector2.up)) > 0.9f)
+            {
+                onJumpableService = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onJumpableService = false;
     }
 }
