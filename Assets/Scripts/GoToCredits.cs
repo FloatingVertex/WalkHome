6b206f94eaf6034c21c7e73c1e245ef27b8b3335
Tom Slavin <tps3871@g.rit.edu>
2018-04-08 17:41:24 -0400

    panic now

---
+++ Assets/Scripts/GoToCredits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToCredits : EventTrigger
+{
+    public override void RunTrigger()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+}
